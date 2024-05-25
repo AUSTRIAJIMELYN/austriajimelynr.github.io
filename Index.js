afd707e438d7c73254f1c238c5033a5dbba27889
AUSTRIAJIMELYN <austriajimelyn@gmail.com>
2024-05-25 18:13:17 +0800

    Create Index.js

---
+++ Index.js
@@ -0,0 +1,7 @@
+const btnLike1 = document.getElementById("btnLike1")
+const countLike1 = document.getElementById("countLike1")
+
+function clickLike1(){
+   let totalLikes = parseInt(countLike1.value) + 1
+   countLike1.textContent = totalLikes.toString()
+}
