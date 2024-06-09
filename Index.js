5a8dbe59d017980368580ca30aefb0c38094b5e1
AUSTRIAJIMELYN <austriajimelyn@gmail.com>
2024-06-09 21:45:35 +0800

    Update Index.js

--- Index.js
+++ Index.js
@@ -16,6 +16,24 @@ function clickLike2(){
 }
 btnLike2.addEventListener("click",clickLike2)
 
+const btnDisLike1 = document.getElementById("btnDisLike1")
+const countDisLike1 = document.getElementById("countDisLike1")
+
+function clickDisLike1(){
+  let totalDisLikes = parseInt(countDisLike1.value) + 1
+  countDisLike1.textContent = totalDisLikes.toString()
+}  
+btnDisLike1.addEventListener("click",clickDisLike1)
+
+const btnDisLike2 = document.getElementById("btnDisLike2")
+const countDisLike2 = document.getElementById("countDisLike2")
+
+function clickDisLike2(){
+  let totalDisLikes = parseInt(countDisLike2.value) + 1
+  countDisLike2.textContent = totalDisLikes.toString()
+}  
+btnDisLike2.addEventListener("click",clickDisLike2)
+
 const comment = document.getElementById("comment")
 const submit = document.getElementById("submit")
 const commentbox = document.getElementById("commentbox")
